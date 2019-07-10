095e4b50bb20170dfce20228b17f308bd9a75b64
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-10 14:51:49 +0800

    implement grouping_count and add method to implement choose_even.

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,7 +2,10 @@
 
 function choose_even(collection) {
 
-
+  //first method
+  // const conditionEven = (num) => num % 2 === 0;
+  // return collection.filter(conditionEven);
+  //second method
   return  collection.filter((number) => number % 2 === 0);
   //implement here
 }
--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,12 @@
 function grouping_count(collection) {
 
   //implement here
+  let result = {};
+  collection.forEach(item =>{
+    result[item] = result[item] >= 1 ? result[item] + 1 : 1;
+  })
+
+  return result;
 }
 
 module.exports = grouping_count;
